dd6e6e758ab697feee057b62c1af0d38b41d9bbd
seannooh <sean.oh0899@gmail.com>
2025-02-01 15:29:26 -0800

    update2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
             <h2>Contact Me</h2>
-            <a href="mailto:sean.oh0899@gmail.com">sean.oh089@gmail.com</a>
+            <a href="mailto:sean.oh0899@gmail.com">sean.oh0899@gmail.com</a>
             <a href="tel:+17145140899">(714) 514-0899</a>
         </div>
     );
